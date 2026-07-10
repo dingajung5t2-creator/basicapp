92e70ccc84cedc47d879de5bd01873b9146e2546
jaeho jung <dingajung5t2@gmail.com>
2026-07-10 11:32:17 +0900

    환경설정

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],//어떤 파일을 검사할지
+  theme: {
+    extend: {},//테마를 어떻게 확장할지
+  },
+  plugins: [],// 플러그인을 사용할지
+}
+
