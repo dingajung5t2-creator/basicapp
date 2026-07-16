0ead2df2d9f9c6ab2ca3800d8dadf9687088bd2f
jaeho jung <dingajung5t2@gmail.com>
2026-07-16 09:30:24 +0900

    json 적용2

--- src/04/MyList.js
+++ src/04/MyList.js
@@ -3,16 +3,18 @@ import MyListItem from "./MyListItem";
 // import "./MyDiv1.css";
 
 export default function MyList() {
-    console.log(MyListData);
+    // console.log(MyListData);
 
     // 4개 배열 요소 하나씩 꺼내서 태그 요소 생성.
     const tags = MyListData.map(item =>
-        <MyListItem 
+        <MyListItem
+            key={item.id}
             title={item.title}
             imgUrl={item.imgUrl}
             content={item.content}
         />
     )
+    console.log("tags: ", tags);
 
     return (
         <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
